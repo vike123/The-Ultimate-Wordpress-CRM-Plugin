58f41b43ea476f4fceb54cb5b97c7a28c079156d
Will Wilson <mojowill@mac.com>
2011-12-08 12:17:29 +0000

    Playing with admin page options
    doesn't actually do anything just trying to work out how I should be storing options!

--- includes/admin/admin-options.php
+++ includes/admin/admin-options.php
@@ -3,51 +3,248 @@
 * Admin area options page
 ***************************/
 
-//Setup the options page
-function uwpcrm_admin_options() {
+// STILL PLAYING WITH THIS BECAUSE I DON'T REALLY KNOW WHAT I'M DOING!
 
-	global $uwpcrm_options;
-	
-	ob_start(); ?>
-	
+
+// ------------------------------------------------------------------------
+// REGISTER HOOKS & CALLBACK FUNCTIONS:                                    
+// ------------------------------------------------------------------------
+// HOOKS TO SETUP DEFAULT PLUGIN OPTIONS, HANDLE CLEAN-UP OF OPTIONS WHEN
+// PLUGIN IS DEACTIVATED AND DELETED, INITIALISE PLUGIN, ADD OPTIONS PAGE.
+// ------------------------------------------------------------------------
+
+// Set-up Hooks
+register_activation_hook(__FILE__, 'uwpcrm_add_defaults');
+register_uninstall_hook(__FILE__, 'uwpcrm_delete_plugin_options');
+add_action('admin_init', 'uwpcrm_init' );
+add_action('admin_menu', 'uwpcrm_add_options_page');
+
+// --------------------------------------------------------------------------------------
+// CALLBACK FUNCTION FOR: register_uninstall_hook(__FILE__, 'uwpcrm_delete_plugin_options')
+// --------------------------------------------------------------------------------------
+// THIS FUNCTION RUNS WHEN THE USER DEACTIVATES AND DELETES THE PLUGIN. IT SIMPLY DELETES
+// THE PLUGIN OPTIONS DB ENTRY (WHICH IS AN ARRAY STORING ALL THE PLUGIN OPTIONS).
+// --------------------------------------------------------------------------------------
+
+// Delete options table entries ONLY when plugin deactivated AND deleted
+function uwpcrm_delete_plugin_options() {
+	delete_option('uwpcrm_options');
+}
+
+// ------------------------------------------------------------------------------
+// CALLBACK FUNCTION FOR: register_activation_hook(__FILE__, 'uwpcrm_add_defaults')
+// ------------------------------------------------------------------------------
+// THIS FUNCTION RUNS WHEN THE PLUGIN IS ACTIVATED. IF THERE ARE NO THEME OPTIONS
+// CURRENTLY SET, OR THE USER HAS SELECTED THE CHECKBOX TO RESET OPTIONS TO THEIR
+// DEFAULTS THEN THE OPTIONS ARE SET/RESET.
+//
+// OTHERWISE, THE PLUGIN OPTIONS REMAIN UNCHANGED.
+// ------------------------------------------------------------------------------
+
+// Define default option settings
+function uwpcrm_add_defaults() {
+	$tmp = get_option('uwpcrm_options');
+    if(($tmp['chk_default_options_db']=='1')||(!is_array($tmp))) {
+		delete_option('uwpcrm_options'); // so we don't have to reset all the 'off' checkboxes too! (don't think this is needed but leave for now)
+		$arr = array(	"chk_button1" => "1",
+						"chk_button3" => "1",
+						"textarea_one" => esc_html( "This type of control allows a large amount of information to be entered all at once. Set the 'rows' and 'cols' attributes to set the width and height." ),
+						"txt_one" => "Enter whatever you like here..",
+						"drp_select_box" => "four",
+						"chk_default_options_db" => "",
+						"rdo_group_one" => "one",
+						"rdo_group_two" => "two"
+		);
+		update_option('uwpcrm_options', $arr);
+	}
+}
+
+// ------------------------------------------------------------------------------
+// CALLBACK FUNCTION FOR: add_action('admin_init', 'uwpcrm_init' )
+// ------------------------------------------------------------------------------
+// THIS FUNCTION RUNS WHEN THE 'admin_init' HOOK FIRES, AND REGISTERS YOUR PLUGIN
+// SETTING WITH THE WORDPRESS SETTINGS API. YOU WON'T BE ABLE TO USE THE SETTINGS
+// API UNTIL YOU DO.
+// ------------------------------------------------------------------------------
+
+// Init plugin options to white list our options
+function uwpcrm_init(){
+	register_setting( 'uwpcrm_plugin_options', 'uwpcrm_options', 'uwpcrm_validate_options' );
+}
+
+// ------------------------------------------------------------------------------
+// CALLBACK FUNCTION FOR: add_action('admin_menu', 'uwpcrm_add_options_page');
+// ------------------------------------------------------------------------------
+// THIS FUNCTION RUNS WHEN THE 'admin_menu' HOOK FIRES, AND ADDS A NEW OPTIONS
+// PAGE FOR YOUR PLUGIN TO THE SETTINGS MENU.
+// ------------------------------------------------------------------------------
+
+// Add menu page
+function uwpcrm_add_options_page() {
+	add_options_page( __( 'Ultimate Wordpress CRM Options', 'uwpcrm' ), __( 'UWPCRM Options', 'uwpcrm' ), 'manage_options', __FILE__, 'uwpcrm_render_form');
+}
+
+// ------------------------------------------------------------------------------
+// CALLBACK FUNCTION SPECIFIED IN: add_options_page()
+// ------------------------------------------------------------------------------
+// THIS FUNCTION IS SPECIFIED IN add_options_page() AS THE CALLBACK FUNCTION THAT
+// ACTUALLY RENDER THE PLUGIN OPTIONS FORM AS A SUB-MENU UNDER THE EXISTING
+// SETTINGS ADMIN MENU.
+// ------------------------------------------------------------------------------
+
+// Render the Plugin options form
+function uwpcrm_render_form() {
+	?>
 	<div class="wrap">
-		<h2><?php echo _e( 'Customer/Reseller Portal Options', 'uwpcrm' );?></h2>
-		<p><?php echo _e( 'This page sets the options for our portal.', 'uwpcrm' );?></p>
 		
+		<!-- Display Plugin Icon, Header, and Description -->
+		<div class="icon32" id="icon-options-general"><br></div>
+		<h2><?php echo _e( 'Plugin Options Starter Kit', 'uwpcrm' );?></h2>
+		<p><?php echo _e( 'Below is a collection of sample controls you can use in your own Plugins. Or, you can analyse the code and learn how all the most common controls can be added to a Plugin options form. See the code for more details, it is fully commented.', 'uwpcrm' );?></p>
+
+		<!-- Beginning of the Plugin Options Form -->
 		<form method="post" action="options.php">
-			
-			<?php settings_fields( 'uwpcrm_settings_group' ); ?>
-			
-			<h4><?php echo _e( 'Enable', 'uwpcrm' );?></h4>
-			<p>
-				<label class="description" for="uwpcrm_settings[enable]"><?php echo _e( 'Enable', 'uwpcrm' );?></label>
-				<input id="uwpcrm_settings[enable]" name="uwpcrm_settings[enable]" type="checkbox" value="1" <?php checked( '1', $uwpcrm_options[ 'enable' ] );?>/>
-			</p>
-			
-			<h4><?php echo _e( 'Twitter Information', 'uwpcrm' );?></h4>
-			<p>
-				<label class="description" for="uwpcrm_settings[twitter_url]"><?php echo _e( 'Enter your Twitter URL', 'uwpcrm' );?></label>
-				<input id="uwpcrm_settings[twitter_url]" name="uwpcrm_settings[twitter_url]" type="text" value="<?php echo $uwpcrm_options[ 'twitter_url' ];?>" />
-			</p>
-			
+			<?php settings_fields('uwpcrm_plugin_options'); ?>
+			<?php $options = get_option('uwpcrm_options'); ?>
+
+			<!-- Table Structure Containing Form Controls -->
+			<!-- Each Plugin Option Defined on a New Table Row -->
+			<table class="form-table">
+
+				<!-- Text Area Control -->
+				<tr>
+					<th scope="row"><?php echo _e( 'Sample Text Area', 'uwpcrm' );?></th>
+					<td>
+						<textarea name="uwpcrm_options[textarea_one]" rows="7" cols="50" type='textarea'><?php echo $options['textarea_one']; ?></textarea><br /><span style="color:#666666;margin-left:2px;"><?php echo _e( 'Add a comment here to give extra information to Plugin users', 'uwpcrm' );?></span>
+					</td>
+				</tr>
+
+				<!-- Textbox Control -->
+				<tr>
+					<th scope="row"><?php echo _e( 'Enter Some Information', 'uwpcrm' );?></th>
+					<td>
+						<input type="text" size="57" name="uwpcrm_options[txt_one]" value="<?php echo $options['txt_one']; ?>" />
+					</td>
+				</tr>
+
+				<!-- Radio Button Group -->
+				<tr valign="top">
+					<th scope="row"><?php echo _e( 'Radio Button Group #1', 'uwpcrm' );?></th>
+					<td>
+						<!-- First radio button -->
+						<label><input name="uwpcrm_options[rdo_group_one]" type="radio" value="one" <?php checked('one', $options['rdo_group_one']); ?> /> <?php echo _e( 'Radio Button #1',' uwpcrm' );?></label><br />
+
+						<!-- Second radio button -->
+						<label><input name="uwpcrm_options[rdo_group_one]" type="radio" value="two" <?php checked('two', $options['rdo_group_one']); ?> /> <?php echo _e( 'Radio Button #2', 'uwpcrm' );?></label><br /><span style="color:#666666;"><?php echo _e( 'General comment to explain more about this Plugin option.', 'uwpcrm' );?></span>
+					</td>
+				</tr>
+
+				<!-- Checkbox Buttons -->
+				<tr valign="top">
+					<th scope="row"><?php echo _e( 'Group of Checkboxes', 'uwpcrm' );?></th>
+					<td>
+						<!-- First checkbox button -->
+						<label><input name="uwpcrm_options[chk_button1]" type="checkbox" value="1" <?php if (isset($options['chk_button1'])) { checked('1', $options['chk_button1']); } ?> /> <?php echo _e( 'Checkbox #1', 'uwpcrm' );?></label><br />
+
+						<!-- Second checkbox button -->
+						<label><input name="uwpcrm_options[chk_button2]" type="checkbox" value="1" <?php if (isset($options['chk_button2'])) { checked('1', $options['chk_button2']); } ?> /> <?php echo _e( 'Checkbox #2', 'uwpcrm' );?></label><br />
+
+						<!-- Third checkbox button -->
+						<label><input name="uwpcrm_options[chk_button3]" type="checkbox" value="1" <?php if (isset($options['chk_button3'])) { checked('1', $options['chk_button3']); } ?> /> <?php echo _e( 'Checkbox #3', 'uwpcrm' );?></label><br />
+
+						<!-- Fourth checkbox button -->
+						<label><input name="uwpcrm_options[chk_button4]" type="checkbox" value="1" <?php if (isset($options['chk_button4'])) { checked('1', $options['chk_button4']); } ?> /> <?php echo _e( 'Checkbox #4', 'uwpcrm' );?> </label><br />
+
+						<!-- Fifth checkbox button -->
+						<label><input name="uwpcrm_options[chk_button5]" type="checkbox" value="1" <?php if (isset($options['chk_button5'])) { checked('1', $options['chk_button5']); } ?> /> <?php echo _e( 'Checkbox #5', 'uwpcrm' );?> </label>
+					</td>
+				</tr>
+
+				<!-- Another Radio Button Group -->
+				<tr valign="top">
+					<th scope="row">Radio Button Group #2</th>
+					<td>
+						<!-- First radio button -->
+						<label><input name="uwpcrm_options[rdo_group_two]" type="radio" value="one" <?php checked('one', $options['rdo_group_two']); ?> /> Radio Button #1</label><br />
+
+						<!-- Second radio button -->
+						<label><input name="uwpcrm_options[rdo_group_two]" type="radio" value="two" <?php checked('two', $options['rdo_group_two']); ?> /> Radio Button #2</label><br />
+
+						<!-- Third radio button -->
+						<label><input name="uwpcrm_options[rdo_group_two]" type="radio" value="three" <?php checked('three', $options['rdo_group_two']); ?> /> Radio Button #3</label>
+					</td>
+				</tr>
+
+				<!-- Select Drop-Down Control -->
+				<tr>
+					<th scope="row">Sample Select Box</th>
+					<td>
+						<select name='uwpcrm_options[drp_select_box]'>
+							<option value='one' <?php selected('one', $options['drp_select_box']); ?>>One</option>
+							<option value='two' <?php selected('two', $options['drp_select_box']); ?>>Two</option>
+							<option value='three' <?php selected('three', $options['drp_select_box']); ?>>Three</option>
+							<option value='four' <?php selected('four', $options['drp_select_box']); ?>>Four</option>
+							<option value='five' <?php selected('five', $options['drp_select_box']); ?>>Five</option>
+							<option value='six' <?php selected('six', $options['drp_select_box']); ?>>Six</option>
+							<option value='seven' <?php selected('seven', $options['drp_select_box']); ?>>Seven</option>
+							<option value='eight' <?php selected('eight', $options['drp_select_box']); ?>>Eight</option>
+						</select>
+						<span style="color:#666666;margin-left:2px;">Add a comment here to explain more about how to use the option above</span>
+					</td>
+				</tr>
+
+				<tr><td colspan="2"><div style="margin-top:10px;"></div></td></tr>
+				<tr valign="top" style="border-top:#dddddd 1px solid;">
+					<th scope="row">Database Options</th>
+					<td>
+						<label><input name="uwpcrm_options[chk_default_options_db]" type="checkbox" value="1" <?php if (isset($options['chk_default_options_db'])) { checked('1', $options['chk_default_options_db']); } ?> /> Restore defaults upon plugin deactivation/reactivation</label>
+						<br /><span style="color:#666666;margin-left:2px;">Only check this if you want to reset plugin settings upon Plugin reactivation</span>
+					</td>
+				</tr>
+			</table>
 			<p class="submit">
-				<input type="submit" class="button-primary" value="Save Options" />
+			<input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
 			</p>
 		</form>
 	</div>
-	
-	<?php echo ob_get_clean();
+	<?php	
 }
 
-//Register the Menu Link
-function uwpcrm_add_options_menu() {
-	add_options_page( __( 'Customer/Reseller Portal Options', 'uwpcrm' ), __( 'WP CRM', 'uwpcrm' ), 'manage_options', 'uwpcrm-options', 'uwpcrm_admin_options' );
+// Sanitize and validate input. Accepts an array, return a sanitized array.
+function uwpcrm_validate_options($input) {
+	 // strip html from textboxes
+	$input['textarea_one'] =  wp_filter_nohtml_kses($input['textarea_one']); // Sanitize textarea input (strip html tags, and escape characters)
+	$input['txt_one'] =  wp_filter_nohtml_kses($input['txt_one']); // Sanitize textbox input (strip html tags, and escape characters)
+	return $input;
 }
 
-add_action( 'admin_menu', 'uwpcrm_add_options_menu' );
+add_filter( 'plugin_action_links', 'uwpcrm_plugin_action_links', 10, 2 );
+// Display a Settings link on the main Plugins page
+function uwpcrm_plugin_action_links( $links, $file ) {
 
-function uwpcrm_register_settings() {
-	register_setting( 'uwpcrm_settings_group', 'uwpcrm_settings' );
+	if ( $file == plugin_basename( __FILE__ ) ) {
+		$uwpcrm_links = '<a href="'.get_admin_url().'options-general.php?page=plugin-options-starter-kit/plugin-options-starter-kit.php">'.__('Settings').'</a>';
+		// make the 'Settings' link appear first
+		array_unshift( $links, $uwpcrm_links );
+	}
+
+	return $links;
 }
 
-add_action( 'admin_init', 'uwpcrm_register_settings');
+// ------------------------------------------------------------------------------
+// SAMPLE USAGE FUNCTIONS:
+// ------------------------------------------------------------------------------
+// THE FOLLOWING FUNCTIONS SAMPLE USAGE OF THE PLUGINS OPTIONS DEFINED ABOVE. TRY
+// CHANGING THE DROPDOWN SELECT BOX VALUE AND SAVING THE CHANGES. THEN REFRESH
+// A PAGE ON YOUR SITE TO SEE THE UPDATED VALUE.
+// ------------------------------------------------------------------------------
+
+// As a demo let's add a paragraph of the select box value to the content output
+add_filter( "the_content", "uwpcrm_add_content" );
+function uwpcrm_add_content($text) {
+	$options = get_option('uwpcrm_options');
+	$select = $options['drp_select_box'];
+	$text = "<p style=\"color: #777;border:1px dashed #999; padding: 6px;\">Select box Plugin option is: {$select}</p>{$text}";
+	return $text;
+}
+//add_action( 'admin_init', 'uwpcrm_register_settings');

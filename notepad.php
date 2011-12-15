1bdb60ef1bb6e6c77ee7a0ba35e4e002e8737e00
Will Wilson <mojowill@mac.com>
2011-12-15 10:19:01 +0000

    Moving a few things around

--- notepad.php
+++ notepad.php
@@ -51,6 +51,18 @@ $uwpcrm_db_version = '1.0';
 //On Plugin activation create the SQL Tables and add DB Version to Options Table
 function uwpcrm_install() {
 	
+	
+/* JUST PLAYING IGNORE
+	global $wpdb, $uwpcrm_db_version;
+	
+	$table_name = $wpdb->prefix . 'uwpcrm_accounts';
+	
+	$sql = "CREATE TABLE  $table_name  (id int(11) NOT NULL AUTO_INCREMENT, time datetime DEFAULT '0000-00-00 00:00:00' NOT NULL, name tinytext NOT NULL, text text NOT NULL, PRIMARY KEY (id) );";
+	
+	$wpdb->query( $sql );
+		
+	add_option( 'uwpcrm_db_version', $uwpcrm_db_version );
+*/
 	//We use this in our setup to create terms!
 	add_option( 'uwpcrm_initial_setup', '1' );
 }
@@ -59,7 +71,21 @@ register_activation_hook( __FILE__, 'uwpcrm_install' );
 
 //Initialise our original data into the Plugin DB Tables and run any data scripts
 function uwpcrm_install_data() {
+	
+/* JUST PLAYING IGNORE
+	global $wpdb;
+	$table_name = $wpdb->prefix . 'uwpcrm_accounts';
 
+	$welcome_name = 'Mr. Wordpress';
+	$welcome_text = 'Congrats, you just made your first plugin write to DB!';
+	
+	$rows_affected = $wpdb->insert( $table_name, array( 
+		'time' => current_time( 'mysql' ),
+		'name' => $welcome_name,
+		'text' => $welcome_text 
+		) 
+	);
+*/
 }
 
 register_activation_hook( __FILE__, 'uwpcrm_install_data' );
@@ -85,6 +111,15 @@ add_action( 'init', 'uwpcrm_setup_terms', 11 );
 //On Plugin Deactivation Drop the tables and remove option reference.
 function uwpcrm_uninstall() {
 
+/* JUST PLAYING IGNORE
+	global $wpdb;
+	$table_name = $wpdb->prefix . 'uwpcrm_accounts';
+	
+	$sql = "DROP TABLE IF EXISTS $table_name;";
+	$wpdb->query( $sql );
+	
+	delete_option( 'uwpcrm_db_version' );
+*/
 }
 
 register_deactivation_hook( __FILE__, 'uwpcrm_uninstall' );
